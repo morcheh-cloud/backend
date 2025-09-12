f13eb902e521fbd1189748a9b3a2c78f4606f6f2
ali <marefati110@gmail.com>
2025-09-12 23:34:21 +0330

    style: format and align code for consistency in module entity

--- src/modules/ansible/entities/module.endity.ts
+++ src/modules/ansible/entities/module.endity.ts
@@ -1,27 +1,33 @@
-import { BaseEntity } from "src/common/base/base.entity"
-import { IsEnumField, IsJSONField } from "src/common/decorators/validation.decorator"
-import type { IPlaybook } from "src/modules/ansible/interfaces/playbook.interface"
-import { Column, Entity } from "typeorm"
+import { BaseEntity } from "src/common/base/base.entity";
+import {
+  IsEnumField,
+  IsJSONField,
+  IsStringField,
+} from "src/common/decorators/validation.decorator";
+import type { IPlaybook } from "src/modules/ansible/interfaces/playbook.interface";
+import { Column, Entity } from "typeorm";
 
 export enum ModuleType {
-	OFFICIAL = "official",
-	COMMUNITY = "community",
-	CUSTOM = "custom",
+  OFFICIAL = "official",
+  COMMUNITY = "community",
+  CUSTOM = "custom",
 }
 
 @Entity()
 export class Module extends BaseEntity {
-	@Column({ unique: true })
-	name!: string
+  @IsStringField()
+  @Column({ unique: true })
+  name!: string;
 
-	@Column()
-	description!: string
+  @IsStringField()
+  @Column()
+  description!: string;
 
-	@IsEnumField(ModuleType)
-	@Column({ enum: ModuleType, type: "enum" })
-	type!: ModuleType
+  @IsEnumField(ModuleType)
+  @Column({ enum: ModuleType, type: "enum" })
+  type!: ModuleType;
 
-	@IsJSONField()
-	@Column({ default: {}, type: "json" })
-	schema!: Partial<IPlaybook>
+  @IsJSONField()
+  @Column({ default: {}, type: "json" })
+  schema!: Partial<IPlaybook>;
 }
