2dae7d7b5d80d52f7d19448d81945bd85b4ead64
ali <marefati110@gmail.com>
2025-09-15 15:08:58 +0330

    feat: replace generic error with ForbiddenException in delete workspace method

--- src/modules/workspace/services/workspace.service.ts
+++ src/modules/workspace/services/workspace.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common"
+import { ForbiddenException, Injectable } from "@nestjs/common"
 import { User } from "src/modules/user/entities/user.entity"
 import { CreateWorkSpacePayload } from "src/modules/workspace/DTOs/workspace.dto"
 import { WorkspaceRepository } from "src/modules/workspace/repositories/workspace.repository"
@@ -21,7 +21,7 @@ export class WorkspaceService {
 	async delete(id: string, userId: string) {
 		const isAllowed = await this.workspaceRepository.findOne({ where: { id, owner: { id: userId } } })
 		if (!isAllowed) {
-			throw new Error("You are not allowed to delete this workspace")
+			throw new ForbiddenException("You are not allowed to delete this workspace")
 		}
 
 		return await this.workspaceRepository.softDelete(id)
